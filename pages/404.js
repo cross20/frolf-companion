e5713ace100e4023a3395ba75dc01228b7a66392
Chad Ross <rosschad98@icloud.com>
2022-04-11 08:44:49 -0500

    Update 404 page to advise trying again

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,9 @@
 export default function Custom404() {
     return (
-        <h1>404 - Page Not Found</h1>
+        <div>
+            <h1>404 - Page Not Found</h1>
+            <p>Please try again and, if the issue persists, good luck.</p>
+        </div>
+        
     )
 }
