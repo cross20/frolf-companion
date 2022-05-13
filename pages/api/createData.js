4e7e9fb78d9615ca120f176244abca547e8ca04b
Chad Ross <rosschad98@icloud.com>
2022-05-12 20:33:25 -0500

    Remove unused database components, apis, and object models. Add toggle between holes when tracking score.

--- pages/api/createData.js
+++
@@ -1,6 +0,0 @@
-import { createData } from '../../lib/redis';
-
-export default async function handler(req, res) {
-    const id = await createData(req.body);
-    res.status(200).json({ id });
-}
--- pages/track-score.js
+++ pages/track-score.js
@@ -8,18 +8,28 @@ import { getHoles } from '../lib/database/redis';
 import styles from '../styles/TrackScore.module.css'
 
 function TrackScore(props) {
-    const [hole, setHole] = useState(props.hole);
+    const course = props.course;
+    const holes = props.holes;
+
+    const [hole, setHole] = useState(0);
 
     return (
         <main>
+            <h1>Track Score</h1>
             <section className={styles.section}>
-                <h2>Hole #{hole.sequence}</h2>
-                <Hole hole={hole} />
+                <h2>Hole {hole + 1}</h2>
+                <Hole hole={holes[hole]} />
             </section>
             <section className={styles.section}>
                 <h2>Throws</h2>
                 <Counter />
             </section>
+            <button onClick={() => {
+                setHole(hole - 1);
+            }} disabled={hole === 0}>Previous Hole</button>
+            <button onClick={() => {
+                setHole(hole + 1);
+            }} disabled={hole === holes.length - 1}>Next Hole</button>
         </main>
     )
 }
@@ -27,12 +37,13 @@ function TrackScore(props) {
 export async function getServerSideProps(context) {
     const holes = await getHoles(0);
 
-    const hole = holes[0].entityData;
-    console.log(hole);
+    holes.sort((a, b) => {
+        return a.sequence - b.sequence;
+    });
 
     return {
         props: {
-            hole,
+            holes,
         }
     }
 }
