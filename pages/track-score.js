90d2225f2749bcaa087e0089717ed1e0af44fff7
Chad Ross <rosschad98@icloud.com>
2022-05-14 15:16:01 -0500

    Fix error caused when no holes exist in the database

--- pages/track-score.js
+++ pages/track-score.js
@@ -10,27 +10,36 @@ import styles from '../styles/TrackScore.module.css'
 function TrackScore(props) {
     const holes = props.holes;
 
-    const [hole, setHole] = useState(0);
+    const [hole, setHole] = useState(0); // Always start on the first hole.
+
+    if (holes && holes.length > 0) {
+        return (
+            <main>
+                <h1>Track Score</h1>
+                <section className={styles.section}>
+                    <h2>Hole {hole + 1}</h2>
+                    <Hole hole={holes[hole]} />
+                </section>
+                <section className={styles.section}>
+                    <h2>Throws</h2>
+                    <Counter />
+                </section>
+                <button onClick={() => {
+                    setHole(hole - 1);
+                }} disabled={hole === 0}>Previous Hole</button>
+                <button onClick={() => {
+                    setHole(hole + 1);
+                }} disabled={hole === holes.length - 1}>Next Hole</button>
+            </main>
+        );
+    }
 
     return (
         <main>
             <h1>Track Score</h1>
-            <section className={styles.section}>
-                <h2>Hole {hole + 1}</h2>
-                <Hole hole={holes[hole]} />
-            </section>
-            <section className={styles.section}>
-                <h2>Throws</h2>
-                <Counter />
-            </section>
-            <button onClick={() => {
-                setHole(hole - 1);
-            }} disabled={hole === 0}>Previous Hole</button>
-            <button onClick={() => {
-                setHole(hole + 1);
-            }} disabled={hole === holes.length - 1}>Next Hole</button>
+            <p>There are no holes in this course.</p>
         </main>
-    )
+    );
 }
 
 export async function getServerSideProps(context) {
