d9509276e0d724397626f9f8b8ff42581c26ba2b
Chad Ross <rosschad98@icloud.com>
2022-09-28 20:35:22 -0700

    Create base search components

---
+++ components/search/search-results.js
@@ -0,0 +1,5 @@
+export default function SearchResults() {
+    return (
+        <div>Search results here</div>
+    );
+}
---
+++ components/search/search.js
@@ -0,0 +1,12 @@
+import SearchBar from "./search-bar";
+import SearchResults from "./search-results";
+
+
+export default function Search() {
+    return (
+        <div>
+            <SearchBar />
+            <SearchResults />
+        </div>
+    );
+}
