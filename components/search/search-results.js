1d1a7b04ea2b0eaf10db640beee547b0844c6732
Chad Ross <rosschad98@icloud.com>
2022-10-22 21:41:01 -0700

    Fix disappearing images when search results used column flex direction

--- components/search/search-results.js
+++ components/search/search-results.js
@@ -4,17 +4,19 @@ import styles from './search-results.module.css'
 
 export default function SearchResults() {
     const [results, setResults] = useState([{urlName: 'whitworth', title: 'Whitworth', description: 'This is the first result.'}, 
-    {urlName: 'whitworth', title: 'Whitworth', description: 'This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result.'}, 
+    {urlName: 'whitworth', title: 'Whitworth', description: 'This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. This is the first result. Testing Testing'}, 
     {urlName: 'whitworth', title: 'Whitworth', description: 'This is the first result.'}, 
     {urlName: 'whitworth', title: 'Whitworth', description: 'This is the first result.'}, 
     {urlName: 'whitworth', title: 'Whitworth', description: 'This is the first result.'}, 
     {urlName: 'whitworth', title: 'Whitworth', description: 'This is the first result.'}, 
     {urlName: 'new-course', title: 'New Course', description: 'This is the second result.'}]);
 
+    let key = 0;
+
     return (
         <div className={styles.container}>
             {results.map((r) => {
-                return <SearchResult result={r} />
+                return <SearchResult result={r} key={key++}/>
             })}
         </div>
     );
